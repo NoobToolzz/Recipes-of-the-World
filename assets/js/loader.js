2aa1451e7fbd3c5477163fbbc774ab56fa574e7d
NoobToolzz <daniyalgam.ertv@gmail.com>
2024-03-02 21:42:04 +1100

    Loader update
    Loader now takes anywhere between 1-3 seconds to load the page. This measure was to add realism.

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -3,7 +3,8 @@
 var myLoader;
 
 function loader() {
-  myVar = setTimeout(showPage, 3000);
+  var randomDelay = Math.floor(Math.random() * (3000 - 1000 + 1)) + 1000; // Random delay between 1000 and 3000 ms
+  myVar = setTimeout(showPage, randomDelay);
 }
 
 function showPage() {
